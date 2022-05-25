f5990901cb7f7ea0cae0e9f6a0e52177ec3ae507
Grace Birnam <gbirnam@alphaworks.tech>
2022-05-25 12:06:42 -0700

    update header.php for fixes

--- header.php
+++ header.php
@@ -12,7 +12,18 @@
 ?>
 <!doctype html>
 <html <?php language_attributes(); ?>>
-<script src="https://kit.fontawesome.com/6b35074062.js" crossorigin="anonymous"></script>
+
+<head>
+	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="profile" href="https://gmpg.org/xfn/11">
+	<script src="https://kit.fontawesome.com/6b35074062.js" crossorigin="anonymous"></script>
+
+	<?php wp_head(); ?>
+</head>
+<body <?php body_class(); ?>>
+<?php wp_body_open(); ?>
+
 	<header id="masthead" class="site-header">
 		<div class="site-branding">
 			<?php
