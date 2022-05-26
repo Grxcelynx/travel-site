4b8ada82b53a3ed0e1e1de187ff9c89421e1d5d0
Grace Birnam <gbirnam@alphaworks.tech>
2022-05-26 08:39:12 -0700

    about page

--- about.php
+++ about.php
@@ -7,12 +7,14 @@
  */ 
 ?> 
 <?php get_header(); ?>
+<div>
 <section class="about-banner">
     <h1>Title HERE</h1>
     <h4>subtitle will go here or tagline</h4>
 </section>
-<section class="who-we-are">
-        <div class="who-text">
+
+    <section class="section-1">
+        <div class="who">
             <h2>WHO ARE WE?</h2>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius hic voluptatibus vel cum nisi nulla deleniti aspernatur doloremque libero dolor. Earum explicabo libero atque neque architecto perspiciatis aliquam quas est.
             Magnam laborum sed dolorem non maxime placeat dolore possimus nisi architecto mollitia, officia harum atque soluta laboriosam voluptas tenetur aliquid dignissimos, dicta minus repellat quis. Illum fugit suscipit molestiae doloremque.Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius hic voluptatibus vel cum nisi nulla deleniti aspernatur doloremque libero dolor. Earum explicabo libero atque neque architecto perspiciatis aliquam quas est.
@@ -22,10 +24,10 @@
             Fuga earum totam harum unde asperiores vitae at adipisci in reprehenderit debitis, quo nisi? Architecto vitae voluptatibus maiores! Sunt vitae nulla nostrum repudiandae dignissimos dolorum deserunt, expedita minima veritatis cupiditate?
            </p>
         </div>
-</section>
-<section class="mission">
+    </section>
 
-    <div class="mission-text">
+    <section class="section-2">
+        <div class="mission">
         <h2>OUR MISSION</h2>
         <p>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris ornare condimentum
@@ -38,8 +40,10 @@
              Proin dignissim purus sed dolor aliquet iaculis.Mauris et imperdiet neque, vel vestibulum tortor. Ut mollis dapibus velit. Integer quis suscipit est. Fusce commodo velit ut pulvinar condimentum. Nulla facilisi. Phasellus mollis purus sit amet quam aliquet, nec pulvinar nisi viverra. Curabitur fringilla ultricies ex quis pellentesque. Ut nisi velit, facilisis in sodales a, dapibus ac ante. Mauris at urna sit amet ex ultrices ma
               amet massa tempor, tempor ligula vel, bibendum lacus. Sed urna massa, aliquam sit amet risus sed, maximus euismod dui. Duis egestas justo leo, eget lobortis dolor suscipit vitae. Integer libero lorem, varius vitae nisi sit amet, malesuada volutpat lectus. Fusce fermentum eget quam in laoreet. Sed tempus placerat nulla. In sodales egestas ex, nec interdum libero aliquam ac. Mauris metus ligula, porttitor in sem sit amet, pretium interdum quam. Integer ornare ipsum et leo sollicitudin, eu varius ligula condimentum. Donec iaculis ligula arcu, eu aliquam ante viverra non. Mauris pharetra vitae ipsum ut eleifend. Fusce dapibus eleifend magna vel tincidunt. Nullam dui felis, dictum dapibus dui a, maximus tempor sapien. Nulla ut tincidunt odio. Nunc pellentesque mauris vel nulla consectetur, luctus finibus dui faucibus. Phasellus mauris augue, congue sit amet elementum in, imperdiet et sapien.
         </p>
-    </div>
-</section>
+        </div>
+    </section>
+</div>
+
 
     <?php get_template_part('template-parts/banner')?> 
     <?php get_footer(); ?>
