2416e4749d2271da0a01ce3ad92044f4a1a4d46d
Grace Birnam <gbirnam@alphaworks.tech>
2022-06-01 10:53:21 -0700

    finished template parts ACF

--- template-parts/flights-hotels.php
+++ template-parts/flights-hotels.php
@@ -1,12 +1,12 @@
 <section class="flights-hotels">
-    <div class="flights">
+    <div class="flights" style="background-image: url(<?php the_field('flight_image'); ?>); background-size: cover; background-position: center;">
         <h2 id="flight-title"><?php the_field('flight_title'); ?></h2>
         <h4 id="flight-subtitle"><?php the_field('flight_subtitle'); ?></h4>
         <img src="" alt="">
         <button id="flight-btn"><?php the_field('flight_button'); ?></button>
     </div>
 
-    <div class="hotels">
+    <div class="hotels" style="background-image: url(<?php the_field('hotel_image'); ?>); background-size: cover; background-position: center;">
         <h2 id="hotel-title"><?php the_field('hotel_title'); ?></h2>
         <h4 id="hotel-subtitle"><?php the_field('hotel_subtitle'); ?></h4>
         <img src="" alt="">
